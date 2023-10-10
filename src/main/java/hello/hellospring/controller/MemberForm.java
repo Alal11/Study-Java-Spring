1ea9d0a6c2141b6c006c2ad9b05ba31cba6447ef
Alal11 <spalqj56@naver.com>
2023-10-10 14:22:07 +0900

    회원 웹 기능 - 등록

---
+++ src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    // Getter and Setter 단축키 : Alt + Insert
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
